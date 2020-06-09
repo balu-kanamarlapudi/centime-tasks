43b6e08aa5488594f87242987fa3c84f2dd7f0ed
E HARISH <erupothu@users.noreply.github.com>
2020-06-09 12:21:47 +0530

    deploy
    deploy from shell

---
+++ allservices-deploy.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# svn checkout http://13.127.98.208/svn/myrepo/firstservice
+cd first-service
+echo "modify permissions"
+chmod 777 log.txt
+chmod 777 pid.file
+
+echo "killing existing server"
+kill $(cat pid.file)
+
+echo "building project"
+mvn package
+
+echo "deploying project"
+nohup java -jar target/first-service.jar > log.txt 2>&1 &
+echo $! > pid.file
+
+cd ..
+cd second-service
+echo "modify permissions"
+chmod 777 log.txt
+chmod 777 pid.file
+
+echo "killing existing server"
+kill $(cat pid.file)
+
+echo "building project"
+mvn package
+
+echo "deploying project"
+nohup java -jar target/second-service.jar > log.txt 2>&1 &
+echo $! > pid.file
+
+cd ..
+cd third-service
+echo "modify permissions"
+chmod 777 log.txt
+chmod 777 pid.file
+
+echo "killing existing server"
+kill $(cat pid.file)
+
+echo "building project"
+mvn package
+
+echo "deploying project"
+nohup java -jar target/third-service.jar > log.txt 2>&1 &
+echo $! > pid.file
+
+cd ..
+cd database-task
+echo "modify permissions"
+chmod 777 log.txt
+chmod 777 pid.file
+
+echo "killing existing server"
+kill $(cat pid.file)
+
+echo "building project"
+mvn package
+
+echo "deploying project"
+nohup java -jar target/database-task.jar > log.txt 2>&1 &
+echo $! > pid.file
+
